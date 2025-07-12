ffc65b8419c931ea90b02ff15c7c0581efb16157
Tanvir Kabir <111991013+pi3d-p1p3r@users.noreply.github.com>
2025-07-12 19:08:08 +0600

    Update A2Q7d.py

--- A2Q7d.py
+++ A2Q7d.py
@@ -20,7 +20,7 @@ print(f"∂Q/∂x = {Qx}")
 print(f"Conservative? {is_conservative}")
 
 # Find potential function φ where F = -∇φ
-phi = x*exp(y)
+phi=sp.integrate(P,x)+sp.integrate(Q-sp.diff(sp.integrate(P,x),y),y)
 
 phi_initial = phi.subs({x: 1, y: 0})
 phi_final = phi.subs({x: -1, y: 0})
